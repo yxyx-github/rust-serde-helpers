fdc7e03a4b4933b9782cd81198f7ddea05432faf
yxyx-github <dev_mw@vodafonemail.de>
2025-09-03 17:38:18 +0200

    test FromXML and ToXML implementations

--- src/xml.rs
+++ src/xml.rs
@@ -70,3 +70,59 @@ where
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+    use crate::xml::test_utils::cleanup_xml;
+    use super::*;
+
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    struct TestData {
+        #[serde(rename = "@attr")]
+        attr: String,
+    }
+
+    const EXPECTED_SERIALIZED: &'static str = r#"<TestData attr="value"/>"#;
+
+    fn expected_deserialized() -> TestData {
+        TestData {
+            attr: "value".into(),
+        }
+    }
+
+    #[test]
+    fn test_from_xml() {
+        let test_data = TestData::from_xml(EXPECTED_SERIALIZED).unwrap();
+        assert_eq!(test_data, expected_deserialized());
+    }
+
+    #[test]
+    fn test_from_xml_file_by_path() {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("some_dir/xml_input_file.xml");
+        fs::create_dir_all(file_path.parent().unwrap()).unwrap();
+        fs::write(&file_path, EXPECTED_SERIALIZED).unwrap();
+
+        let test_data = TestData::from_xml_file_by_path(&file_path).unwrap();
+
+        assert_eq!(test_data, expected_deserialized());
+    }
+
+    #[test]
+    fn test_to_xml() {
+        let xml = expected_deserialized().to_xml(false).unwrap();
+        assert_eq!(xml, cleanup_xml(EXPECTED_SERIALIZED.into()));
+    }
+
+    #[test]
+    fn test_to_xml_file_by_path() {
+        let tmp_dir = tempdir().unwrap();
+        let file_path = tmp_dir.path().join("some_dir/xml_output_file.xml");
+
+        expected_deserialized().to_xml_file_by_path(&file_path, false).unwrap();
+
+        let xml = fs::read_to_string(&file_path).unwrap();
+        assert_eq!(xml, cleanup_xml(EXPECTED_SERIALIZED.into()));
+    }
+}
