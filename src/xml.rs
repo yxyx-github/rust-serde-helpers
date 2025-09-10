9d2d69ab4d13d99a9bc35135b02a39e82f3b2461
yxyx-github <dev_mw@vodafonemail.de>
2025-09-10 07:55:32 +0200

    implement pretty print

--- src/xml.rs
+++ src/xml.rs
@@ -1,7 +1,8 @@
-use std::{fs, io};
-use std::path::Path;
-use quick_xml::{de, se, DeError, SeError};
+use quick_xml::se::Serializer;
+use quick_xml::{de, DeError, SeError};
 use serde::{Deserialize, Serialize};
+use std::path::Path;
+use std::{fs, io};
 use thiserror::Error;
 
 pub mod test_utils;
@@ -58,13 +59,18 @@ where
     T: Serialize,
 {
     fn to_xml(&self, declaration_header: bool) -> Result<String, SeError> {
-        if declaration_header {
-            let mut serialized = String::from(r#"<?xml version="1.0" encoding="UTF-8"?>"#);
-            serialized.push_str(&se::to_string(self)?);
-            Ok(serialized)
+        let mut serialized_content = String::new();
+        let mut ser = Serializer::new(&mut serialized_content);
+        ser.indent(' ', 2);
+        self.serialize(ser)?;
+        // let serialized_content = se::to_string(self)?;
+        Ok(if declaration_header {
+            let mut serialized = String::from(concat!(r#"<?xml version="1.0" encoding="UTF-8"?>"#, "\n"));
+            serialized.push_str(&serialized_content);
+            serialized
         } else {
-            se::to_string(self)
-        }
+            serialized_content
+        })
     }
 
     fn to_xml_file_by_path<P: AsRef<Path>>(&self, path: P, declaration_header: bool) -> Result<(), WriteXMLFileError> {
@@ -80,27 +86,56 @@ where
 
 #[cfg(test)]
 mod tests {
-    use tempfile::tempdir;
-    use crate::xml::test_utils::cleanup_xml;
     use super::*;
+    use crate::xml::test_utils::cleanup_xml;
+    use tempfile::tempdir;
 
     #[derive(Serialize, Deserialize, PartialEq, Debug)]
     struct TestData {
         #[serde(rename = "@attr")]
         attr: String,
+
+        #[serde(rename = "TestDataChild")]
+        child: TestDataChild,
     }
 
-    const EXPECTED_SERIALIZED: &'static str = r#"<TestData attr="value"/>"#;
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    struct TestDataChild {
+        #[serde(rename = "@attr")]
+        attr: String,
+    }
+
+    const EXPECTED_SERIALIZED: &'static str = r#"
+        <TestData attr="value">
+            <TestDataChild attr="child value"/>
+        </TestData>
+    "#;
+
+    const EXPECTED_SERIALIZED_WITH_DECLARATION_HEADER: &'static str = r#"
+        <?xml version="1.0" encoding="UTF-8"?>
+        <TestData attr="value">
+            <TestDataChild attr="child value"/>
+        </TestData>
+    "#;
 
     fn expected_deserialized() -> TestData {
         TestData {
             attr: "value".into(),
+            child: TestDataChild {
+                attr: "child value".into(),
+            }
         }
     }
 
     #[test]
     fn test_from_xml() {
         let test_data = TestData::from_xml(EXPECTED_SERIALIZED).unwrap();
+        assert_eq!(test_data, expected_deserialized());
+    }
+
+    #[test]
+    fn test_from_xml_with_declaration_header() {
+        let test_data = TestData::from_xml(EXPECTED_SERIALIZED_WITH_DECLARATION_HEADER).unwrap();
         assert_eq!(test_data, expected_deserialized());
     }
 
@@ -119,7 +154,13 @@ mod tests {
     #[test]
     fn test_to_xml() {
         let xml = expected_deserialized().to_xml(false).unwrap();
-        assert_eq!(xml, cleanup_xml(EXPECTED_SERIALIZED.into()));
+        assert_eq!(cleanup_xml(xml), cleanup_xml(EXPECTED_SERIALIZED.into()));
+    }
+
+    #[test]
+    fn test_to_xml_with_declaration_header() {
+        let xml = expected_deserialized().to_xml(true).unwrap();
+        assert_eq!(cleanup_xml(xml), cleanup_xml(EXPECTED_SERIALIZED_WITH_DECLARATION_HEADER.into()));
     }
 
     #[test]
@@ -130,6 +171,6 @@ mod tests {
         expected_deserialized().to_xml_file_by_path(&file_path, false).unwrap();
 
         let xml = fs::read_to_string(&file_path).unwrap();
-        assert_eq!(xml, cleanup_xml(EXPECTED_SERIALIZED.into()));
+        assert_eq!(cleanup_xml(xml), cleanup_xml(EXPECTED_SERIALIZED.into()));
     }
 }
