e59c44fdf1dca627b712127fd8d13bca6e98f681
yxyx-github <dev_mw@vodafonemail.de>
2026-04-19 13:06:12 +0200

    refactor IsDefault, add test

--- src/default.rs
+++ src/default.rs
@@ -1,7 +1,21 @@
 pub trait IsDefault: Default + PartialEq {
     fn is_default(&self) -> bool {
-        self == &Default::default()
+        *self == Default::default()
     }
 }
 
 impl<T: Default + PartialEq> IsDefault for T {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_default() {
+        assert!(String::from("").is_default());
+        assert!(!String::from("ABC").is_default());
+
+        assert!(&String::from("").is_default());
+        assert!(!&String::from("ABC").is_default());
+    }
+}
